f12c6f61d9cb9c2553ce9bca366854c519093583
Joe Landers <joe@joelanders.net>
2021-06-09 22:26:35 +0200

    Initial Commit

---
+++ dataflow-toy/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  dataflow-toy
+//
+//  Created by Joe Landers on 09.06.21.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ dataflow-toy/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  dataflow-toy
+//
+//  Created by Joe Landers on 09.06.21.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
